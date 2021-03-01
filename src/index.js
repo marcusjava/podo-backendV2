eae9e3d248ea83d1cf5e5e32d5d102c9af35a73b
marcusjava <marcusbuba@hotmail.com>
2021-02-28 21:29:55 -0300

    front

--- src/index.js
+++ src/index.js
@@ -77,7 +77,7 @@ app.use(
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(__dirname, "..", "client", "build")));
-  app.get("/app", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(
       path.resolve(__dirname, "..", "client", "build", "index.html")
     );
