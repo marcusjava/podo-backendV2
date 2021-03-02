a634d19d3f171e76421a94d60acf8d93bddf5643
marcusjava <marcusbuba@hotmail.com>
2021-03-01 22:28:41 -0300

    tentativa de deploy

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.44d06a85db14cc144b20e845121088a0.js"
+  "/precache-manifest.98d7077a6fa5386865fa1cb7b5efcc0c.js"
 );
 
 self.addEventListener('message', (event) => {
