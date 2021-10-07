06dbcee3db807869ccc76f266e8785de6f5b8026
Marcus Vinicius <buba.sergipe@gmail.com>
2021-10-07 15:39:28 -0300

    resolvido erro cadastro cliente

--- src/controllers/ClientController.js
+++ src/controllers/ClientController.js
@@ -3,7 +3,6 @@ const Consult = require("../models/Consult");
 const ValidateClient = require("../validation/client");
 
 const create = async (req, res, next) => {
-  console.log(req.file);
   const { errors, isValid } = ValidateClient(req.body);
 
   if (!isValid) {
@@ -23,7 +22,7 @@ const create = async (req, res, next) => {
     etnia,
   } = req.body;
   const newClient = new Client({
-    avatar: req.file === "undefined" ? "no-img.png" : req.file.key,
+    avatar: req.file === undefined ? "no-img.png" : req.file.key,
     name,
     instagram,
     cpf,
